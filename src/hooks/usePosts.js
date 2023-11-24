f3d5372f1b4f2d1cfc98802aaeca8bc285f0689f
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-24 19:58:13 +0100

    a

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -4,7 +4,7 @@ import { getPostsPage } from "../api/axios";
 
 
 const usePosts = (pageNum = 1) => {
-    const [ results, setResults ] = useState([])
+    const [ results, setResults ] = useState([]);
     const [ loading, setLoading ] = useState(false);
     const [ error, setError ] = useState({});
     const [ isError, setIsError ] = useState(false);
